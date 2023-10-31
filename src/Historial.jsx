689c8b9aba7e6a99fc5d93d9ef6ed41f20f5494e
Andrea Lopez <skarletswdev@gmail.com>
2023-10-31 10:41:52 -0300

    tabla + boton volver

--- src/Historial.jsx
+++ src/Historial.jsx
@@ -1,6 +1,33 @@
 function Historial() {
     return (
-        <>Historial
+        <>
+            <h1>Historial de cotizaciones</h1>
+
+            <div>
+                <table>
+                    <thead>
+                        <th>Fecha de cotización</th>
+                        <th>Propiedad</th>
+                        <th>Ubicación</th>
+                        <th>Metros cuadrados</th>
+                        <th>Póliza mensual</th>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>...</td>
+                            <td>...</td>
+                            <td>...</td>
+                            <td>...</td>
+                            <td>...</td>
+                        </tr>
+                    </tbody>
+                </table>
+
+                <section>
+                    <button>Volver</button>
+                </section>
+
+            </div>
         </>
     )
 }
